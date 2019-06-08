8c86d84db7b5539d1f64a82c21eddbb57b87b28e
VIGNESH S <43236664+VICKY129@users.noreply.github.com>
2019-06-08 14:51:56 +0530

    Update Vowel_Consonant.java

--- Vowel_Consonant.java
+++ Vowel_Consonant.java
@@ -4,14 +4,11 @@ public class Vowel_Consonant {
 		Scanner scan=new Scanner(System.in);
 		String str=scan.next();
 		char ch1[]=str.toCharArray();
-		String v="",c="",n="";
 		for(int i=0;i<ch1.length;i++){
 		if(ch1[i]=='a' || ch1[i]=='e' || ch1[i]=='i' || ch1[i]=='o'||ch1[i]=='u'){
-			v+=String.valueOf(ch1[i]);
 			System.out.println("Vowel");
 		}
 		else if(Character.isAlphabetic(ch1[i])&&(!Character.isDigit(ch1[i]))){
-			c+=String.valueOf(ch1[i]);
 			System.out.println("Consonant");
 		}
 		else{
