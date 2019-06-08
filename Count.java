22a32fe6697b2d14df58c4a3ecd3f00750b108d2
VIGNESH S <43236664+VICKY129@users.noreply.github.com>
2019-06-08 16:22:57 +0530

    Create Count.java

---
+++ Count.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Count {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String n=scan.next();
+		System.out.println(n.length());
+		
+	}
+
+}
